48be11f097ac4f6a485e8eb253cd608a37a40b69
lgarcin <laurent.garcin@gmail.com>
2017-07-21 08:14:14 +0200

    Arithmetics

--- Arithmetics.hs
+++ Arithmetics.hs
@@ -1,2 +1,9 @@
-decomp n b=
-  if n==0 then [] else n%b::(decomp n//b b)
+module Main where
+
+decomp :: Integer -> Integer -> [Integer]
+decomp 0 _ = []
+decomp n b = rem n b : decomp (div n b) b
+
+main::IO()
+main=
+  putStrLn "hello, world"
